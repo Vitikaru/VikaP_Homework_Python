f14ca16de91167245b539ce5a870477e2d9fbc8a
Vitikaru <przhanovaviktoriya99@gmail.com>
2025-02-13 18:25:44 +0500

    ok

--- 05_lesson/Клик по кнопке.py
+++ 05_lesson/Клик по кнопке.py
@@ -3,14 +3,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome(
     service=ChromeService(ChromeDriverManager().install()))
 driver.get("http://the-internet.herokuapp.com/add_remove_elements/")
 click = '[onclick="addElement()"]'
-search_input = driver.find_element(By.CSS_SELECTOR, click)
+button = driver.find_element(By.CSS_SELECTOR, click)
 for x in range(1, 6):
-    search_input.send_keys(click, Keys.ENTER)
+    button.click()
 delete = driver.find_elements(By.CSS_SELECTOR, '[onclick="deleteElement()"]')
 print(len(delete))
 sleep(10)
