c6655ac2ea5b8676218b077b3b32c87b93ff4516
Vitikaru <przhanovaviktoriya99@gmail.com>
2025-02-14 11:34:07 +0500

    ok

--- 05_lesson/Модальное окно.py
+++ 05_lesson/Модальное окно.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Firefox(
     service=FirefoxService(GeckoDriverManager().install()))
 driver.get("http://the-internet.herokuapp.com/entry_ad")
+sleep(5)
 close_button = driver.find_element(By.CSS_SELECTOR, 'div.modal-footer > p')
 close_button.click()
 sleep(10)
