a13f25a95ca1f8276b91ec1bcd022c45d8781224
Vitikaru <przhanovaviktoriya99@gmail.com>
2025-02-18 14:30:29 +0500

    ok

---
+++ 06_lesson/Нажать на кнопку.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome(
+    service=ChromeService(ChromeDriverManager().install()))
+driver.implicitly_wait(20)
+driver.get("http://uitestingplayground.com/ajax")
+click = "#ajaxButton"
+driver.find_element(By.CSS_SELECTOR, click).click()
+content = driver.find_element(By.CSS_SELECTOR, "#content")
+txt = content.find_element(By.CSS_SELECTOR, "p.bg-success").text
+print(txt)
+driver.quit()
