25ba46079eac597230c738c1815c1543f73c489d
Vitikaru <przhanovaviktoriya99@gmail.com>
2025-02-22 22:32:44 +0500

    ok

---
+++ 06K_lesson/test_01_form.py
@@ -0,0 +1,76 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome(service=ChromeService(
+        ChromeDriverManager().install()))
+    yield driver
+    driver.quit()
+
+
+def test(driver):
+    driver.get(
+        "https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
+
+    def send_keys():
+        driver.find_element(
+            By.CSS_SELECTOR, 'input[name="first-name"]').send_keys("Иван")
+        driver.find_element(
+            By.CSS_SELECTOR, 'input[name="last-name"]').send_keys("Петров")
+        driver.find_element(
+            By.CSS_SELECTOR, 'input[name="address"]').send_keys("Ленина, 55-3")
+        driver.find_element(By.CSS_SELECTOR, 'input[name="e-mail"]'
+                            ).send_keys("test@skypro.com")
+        driver.find_element(
+            By.CSS_SELECTOR, 'input[name="phone"]').send_keys("+7985899998787")
+        driver.find_element(
+            By.CSS_SELECTOR, 'input[name="zip-code"]').send_keys("")
+        driver.find_element(
+            By.CSS_SELECTOR, 'input[name="city"]').send_keys("Москва")
+        driver.find_element(
+            By.CSS_SELECTOR, 'input[name="country"]').send_keys("Россия")
+        driver.find_element(
+            By.CSS_SELECTOR, 'input[name="job-position"]').send_keys("QA")
+        driver.find_element(
+            By.CSS_SELECTOR, 'input[name="company"]').send_keys("SkyPro")
+
+    driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+
+    def color():
+        assert "alert py-2 alert-danger" in driver.find_element(
+            By.CSS_SELECTOR, "#zip-code"
+            ).get_attribute("class")
+        assert "alert py-2 alert-success" in driver.find_element(
+            By.CSS_SELECTOR, "#first-name"
+            ).get_attribute("class")
+        assert "alert py-2 alert-success" in driver.find_element(
+            By.CSS_SELECTOR, "#last-name"
+            ).get_attribute("class")
+        assert "alert py-2 alert-success" in driver.find_element(
+            By.CSS_SELECTOR, "#address"
+            ).get_attribute("class")
+        assert "alert py-2 alert-success" in driver.find_element(
+            By.CSS_SELECTOR, "#city"
+            ).get_attribute("class")
+        assert "alert py-2 alert-success" in driver.find_element(
+            By.CSS_SELECTOR, "#country"
+            ).get_attribute("class")
+        assert "alert py-2 alert-success" in driver.find_element(
+            By.CSS_SELECTOR, "#e-mail"
+            ).get_attribute("class")
+        assert "alert py-2 alert-success" in driver.find_element(
+            By.CSS_SELECTOR, "#phone"
+            ).get_attribute("class")
+        assert "alert py-2 alert-success" in driver.find_element(
+            By.CSS_SELECTOR, "#job-position"
+            ).get_attribute("class")
+        assert "alert py-2 alert-success" in driver.find_element(
+            By.CSS_SELECTOR, "#company"
+            ).get_attribute("class")
+
+    driver.quit()
---
+++ 06K_lesson/test_02_calc.py
@@ -0,0 +1,43 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome(service=ChromeService(
+        ChromeDriverManager().install()))
+    yield driver
+    driver.quit()
+
+
+def test(driver):
+    driver.get(
+        "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+
+    driver.find_element(By.CSS_SELECTOR, "#delay").clear()
+    driver.find_element(By.CSS_SELECTOR, "#delay").send_keys(45)
+
+    driver.find_element(
+        By.CSS_SELECTOR, "#calculator > div.keys > span:nth-child(1)").click()
+    driver.find_element(
+        By.CSS_SELECTOR, "#calculator > div.keys > span:nth-child(4)").click()
+    driver.find_element(
+        By.CSS_SELECTOR, "#calculator > div.keys > span:nth-child(2)").click()
+    driver.find_element(
+        By.CSS_SELECTOR,
+        "#calculator > div.keys > span.btn.btn-outline-warning").click()
+
+    WebDriverWait(driver, 45).until(
+        EC.text_to_be_present_in_element(
+            (By.CSS_SELECTOR, '[class="screen"]'), "15")
+        )
+
+    assert "15" in driver.find_element(
+        By.CSS_SELECTOR, '[class="screen"]').text
+
+    driver.quit()
